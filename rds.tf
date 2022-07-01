3b9d4f485753f11ed79edab1a348486a0027cce9
Andreas Vikke <azv@efio.dk>
2022-07-01 13:08:41 +0200

    Added optional replicate_source_db option

--- rds.tf
+++ rds.tf
@@ -1,18 +1,27 @@
 resource "aws_db_instance" "this" {
-  identifier             = "${local.name_prefix}db"
-  allocated_storage      = local.config.volume_size
-  engine                 = local.config.engine
-  engine_version         = local.config.engine_version
-  instance_class         = local.config.instance_type
-  db_name                = local.config.db_name
-  username               = local.config.username
-  password               = aws_ssm_parameter.this.value
-  copy_tags_to_snapshot  = true
-  db_subnet_group_name   = aws_db_subnet_group.this.id
-  vpc_security_group_ids = [aws_security_group.instance.id]
-  multi_az               = local.config.multi_az
+  identifier              = "${local.name_prefix}${local.config.instance_name}"
+  allocated_storage       = local.config.volume_size
+  engine                  = local.engine
+  engine_version          = local.engine_version
+  instance_class          = local.config.instance_type
+  db_name                 = local.config.db_name
+  username                = local.username
+  password                = local.password
+  copy_tags_to_snapshot   = true
+  db_subnet_group_name    = aws_db_subnet_group.this.id
+  vpc_security_group_ids  = [aws_security_group.instance.id]
+  multi_az                = local.config.multi_az
+  replicate_source_db     = local.config.replicate_source_db
+  backup_retention_period = local.backup_retention_period
+
 
   tags = local.default_tags
+
+  lifecycle {
+    ignore_changes = [
+      replicate_source_db
+    ]
+  }
 }
 
 resource "aws_db_subnet_group" "this" {
