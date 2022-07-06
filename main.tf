bc8912f58ac30706d1ed1ffd38487065f7778221
Joachim Rorbol <jr@efio.dk>
2022-07-06 18:42:24 +0200

    Enabled kms encryption (optional) + backup and maintaince window parameters

--- main.tf
+++ main.tf
@@ -1,13 +1,15 @@
 
 locals {
   config = defaults(var.config, {
-    port           = 3306
-    engine         = "mysql"
-    engine_version = "8.0"
-    instance_type  = "db.t3.small"
-    volume_size    = 30
-    multi_az       = false
-    username       = "admin"
+    port               = 3306
+    engine             = "mysql"
+    engine_version     = "8.0"
+    instance_type      = "db.t3.small"
+    volume_size        = 30
+    multi_az           = false
+    username           = "admin"
+    backup_window      = "01:30-02:59"
+    maintenance_window = "Sat:03:00-Sat:04:00"
   })
 
   major_engine_version = split(".", local.config.engine_version)[0]
--- rds.tf
+++ rds.tf
@@ -1,16 +1,20 @@
 resource "aws_db_instance" "this" {
-  identifier             = "${local.name_prefix}db"
-  allocated_storage      = local.config.volume_size
-  engine                 = local.config.engine
-  engine_version         = local.config.engine_version
-  instance_class         = local.config.instance_type
-  db_name                = local.config.db_name
-  username               = local.config.username
-  password               = aws_ssm_parameter.this.value
-  copy_tags_to_snapshot  = true
-  db_subnet_group_name   = aws_db_subnet_group.this.id
-  vpc_security_group_ids = [aws_security_group.instance.id]
-  multi_az               = local.config.multi_az
+  identifier              = "${local.name_prefix}db"
+  allocated_storage       = local.config.volume_size
+  engine                  = local.config.engine
+  engine_version          = local.config.engine_version
+  instance_class          = local.config.instance_type
+  db_name                 = local.config.db_name
+  username                = local.config.username
+  password                = aws_ssm_parameter.this.value
+  copy_tags_to_snapshot   = true
+  db_subnet_group_name    = aws_db_subnet_group.this.id
+  vpc_security_group_ids  = [aws_security_group.instance.id]
+  multi_az                = local.config.multi_az
+  backup_retention_period = 35
+  backup_window           = local.config.backup_window
+  maintenance_window      = local.config.maintenance_window
+  # kms_key_id              = local.kms_key
 
   tags = local.default_tags
 }
@@ -22,25 +26,3 @@ resource "aws_db_subnet_group" "this" {
 
   tags = local.default_tags
 }
-
-# resource "aws_db_option_group" "this" {
-#   name                     =  "${local.name_prefix}option-group"
-#   # option_group_description = "Terraform Option Group"
-#   engine_name              = local.config.engine
-#   major_engine_version     = local.major_engine_version
-
-#   option {
-#     option_name = "MARIADB_AUDIT_PLUGIN"
-
-#     option_settings = [
-#       {
-#         name  = "SERVER_AUDIT_EVENTS"
-#         value = "CONNECT"
-#       },
-#       {
-#         name  = "SERVER_AUDIT_FILE_ROTATIONS"
-#         value = "37"
-#       },
-#     ]
-#   }
-# }
