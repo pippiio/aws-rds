5527990fb8ce6db8e0ec89011c2ecd84b362dbfa
Joachim Rorbol <jr@efio.dk>
2024-01-09 10:26:58 +0100

    Issue w. postgres logging

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -1,26 +1,26 @@
 resource "aws_cloudwatch_log_group" "audit" {
-  name              = "/aws/rds/instance/${local.name_prefix}db/audit"
+  name              = "/aws/rds/instance/${local.name_prefix}${var.config.instance_name}/audit"
   retention_in_days = 7
   kms_key_id        = local.kms_key
   tags              = local.default_tags
 }
 
 resource "aws_cloudwatch_log_group" "error" {
-  name              = "/aws/rds/instance/${local.name_prefix}db/error"
+  name              = "/aws/rds/instance/${local.name_prefix}${var.config.instance_name}/error"
   retention_in_days = 7
   kms_key_id        = local.kms_key
   tags              = local.default_tags
 }
 
 resource "aws_cloudwatch_log_group" "general" {
-  name              = "/aws/rds/instance/${local.name_prefix}db/general"
+  name              = "/aws/rds/instance/${local.name_prefix}${var.config.instance_name}/general"
   retention_in_days = 7
   kms_key_id        = local.kms_key
   tags              = local.default_tags
 }
 
 resource "aws_cloudwatch_log_group" "slowquery" {
-  name              = "/aws/rds/instance/${local.name_prefix}db/slowquery"
+  name              = "/aws/rds/instance/${local.name_prefix}${var.config.instance_name}/slowquery"
   retention_in_days = 7
   kms_key_id        = local.kms_key
   tags              = local.default_tags
